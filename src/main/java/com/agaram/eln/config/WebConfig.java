f0e2539683896fc91dce6bb3c397a3c158992d39
Mudhasir <syedmudhasir.m@agaramtech.com>
2021-04-23 17:59:06 +0530

    new build

---
+++ src/main/java/com/agaram/eln/config/WebConfig.java
@@ -0,0 +1,30 @@
+package com.agaram.eln.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+@EnableWebMvc
+public class WebConfig extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+    	registry.addMapping("/**").allowedMethods("GET", "POST");
+//    	registry.addMapping("/api/**")
+//        .allowedOrigins("http://domain2.com")
+//        .allowedMethods("HEAD", "GET", "PUT", "POST", "DELETE", "PATCH", "OPTIONS")
+//        .allowedHeaders("X-TenantID", "header2", "header3")
+//        .exposedHeaders("X-TenantID", "header2")
+//        .allowCredentials(false).maxAge(3600);
+    }
+    
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/ELNdocuments/").setViewName("forward:/resources/static/ELNdocuments/");
+//        registry.addViewController("/docs/").setViewName("forward:/docs/*");
+    super.addViewControllers(registry);
+    }
+}
